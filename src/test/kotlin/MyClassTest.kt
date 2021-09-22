fc7c8ace19841a9c3ef043e3150ddb2d85ab48e6
User <bilyal.korolkov@gmail.com>
2021-09-22 12:21:27 +0300

    allure

--- src/test/kotlin/MyClassTest.kt
+++ src/test/kotlin/MyClassTest.kt
@@ -2,31 +2,42 @@ import com.codeborne.selenide.CollectionCondition.sizeGreaterThan
 import com.codeborne.selenide.Condition.text
 import com.codeborne.selenide.Configuration
 import com.codeborne.selenide.Selenide.*
+import com.codeborne.selenide.WebDriverRunner
 import com.codeborne.selenide.logevents.SelenideLogger
 import io.qameta.allure.selenide.AllureSelenide
 import org.openqa.selenium.By
-import org.testng.annotations.AfterMethod
+import org.openqa.selenium.chrome.ChromeDriver
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
 
 class MyClassTest {
 
-    @BeforeMethod
+    @BeforeClass
     fun setUp() {
-        println("Test started")
         SelenideLogger.addListener("AllureSelenide", AllureSelenide().screenshots(true).savePageSource(true))
+        Configuration.startMaximized = true
     }
 
-    @AfterMethod
-    fun tearDown() {
-        SelenideLogger.removeListener<AllureSelenide>("AllureSelenide")
-        println("Test finished")
+    @BeforeMethod
+    fun setUpMethod() {
+        val myWebDriver = ChromeDriver()
+        WebDriverRunner.setWebDriver(myWebDriver)
     }
 
     @Test
     fun testMyFun() {
-        Configuration.startMaximized = true
+        println(Thread.currentThread().id)
+        open("https://google.com/ncr")
+        element(By.name("q")).setValue("selenide").pressEnter()
+        elements(By.cssSelector("#res .g")).shouldHave(sizeGreaterThan(5))
+        element(By.cssSelector("#res .g")).shouldHave(text("concise ui tests in Java"))
+    }
+
+    @Test
+    fun testMyFun2() {
+        println(Thread.currentThread().id)
         open("https://google.com/ncr")
         element(By.name("q")).setValue("selenide").pressEnter()
         elements(By.cssSelector("#res .g")).shouldHave(sizeGreaterThan(5))
