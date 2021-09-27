e877011ca33dfa0ca55fc10f210b6de7d4022d82
User <bilyal.korolkov@gmail.com>
2021-09-27 13:23:13 +0300

    allure testNg reporting

--- src/test/kotlin/ui/C12345.kt
+++ src/test/kotlin/ui/C12345.kt
@@ -1,26 +1,27 @@
+package ui
+
 import com.codeborne.selenide.CollectionCondition.sizeGreaterThan
 import com.codeborne.selenide.Condition.text
-import com.codeborne.selenide.WebDriverRunner.driver
 import com.codeborne.selenide.logevents.SelenideLogger
-import io.qameta.allure.*
+import io.qameta.allure.Description
+import io.qameta.allure.Severity
+import io.qameta.allure.SeverityLevel
+import io.qameta.allure.TmsLink
 import io.qameta.allure.selenide.AllureSelenide
 import io.qameta.allure.selenide.LogType
 import mu.KLogging
-import org.openqa.selenium.logging.LogEntries
-import org.openqa.selenium.logging.LogEntry
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 import org.testng.asserts.Assertion
 import pages.GoogleStartPage.searchResults
 import steps.searchQueryResult
-import java.util.*
 import java.util.logging.Level
-
 
 class C12345 {
     companion object : KLogging()
 
-    @BeforeMethod
+    @BeforeClass
     fun setUpMethod() {
         SelenideLogger.addListener(
             "AllureSelenide",
@@ -28,7 +29,12 @@ class C12345 {
                 .includeSelenideSteps(true)
                 .screenshots(true)
                 .savePageSource(true)
+                .enableLogs(LogType.BROWSER, Level.ALL)
                 .enableLogs(LogType.CLIENT, Level.ALL)
+                .enableLogs(LogType.DRIVER, Level.ALL)
+                .enableLogs(LogType.PERFORMANCE, Level.ALL)
+                .enableLogs(LogType.PROFILER, Level.ALL)
+                .enableLogs(LogType.SERVER, Level.ALL)
         )
     }
 
@@ -54,33 +60,21 @@ class C12345 {
 
         searchQueryResult("selenide")
 
-        val logEntries: LogEntries = driver().webDriver.manage().logs().get(LogType.BROWSER.toString())
-        val logs = StringBuilder()
-        for (entry: LogEntry in logEntries) {
-            logs.append(
-                Date(entry.timestamp).toString() + " "
-                        + entry.level + " " + entry.message
-            )
-            logs.append(System.lineSeparator())
-        }
-        Allure.addAttachment("Console log: ", logs.toString())
+        Assertion().assertTrue(searchResults.size > 5)
+        searchResults.shouldHave(sizeGreaterThan(5))
+        searchResults[0].shouldHave(text("concise ui tests in Java1"))
+    }
+
+    @Test(description = "Название тестового метода")
+    @Description("Подробное описание тестовго метода")
+    @TmsLink("https://testrail.app.local/testrail/index.php?/auth/login/")
+    @Severity(SeverityLevel.BLOCKER)
+    fun testMyFun2() {
+        logger.info { "Открываем стартовую страницу" }
+        searchQueryResult("selenide")
 
         Assertion().assertTrue(searchResults.size > 5)
         searchResults.shouldHave(sizeGreaterThan(5))
         searchResults[0].shouldHave(text("concise ui tests in Java"))
     }
-
-//    @Test(groups = ["smoke"])
-//    @Description("Описание тестового метода testMyFun2")
-//    fun testMyFun2() {
-//        logger.info { "Открываем стартовую страницу" }
-//        open("https://google.com/ncr")
-//
-//        queryInput.value = "selenide"
-//        queryInput.pressEnter()
-//
-//        Assertion().assertTrue(searchResults.size > 5)
-//        searchResults.shouldHave(sizeGreaterThan(5))
-//        searchResults[0].shouldHave(text("concise ui tests in Java"))
-//    }
 }
